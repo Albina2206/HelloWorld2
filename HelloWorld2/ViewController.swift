bea43bc129373c05e4c1fbbcacba8cb4938a2c73
Albina <albisha92@icloud.com>
2023-01-30 20:51:14 +0200

    Initial Commit

---
+++ HelloWorld2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by Albina Rysbaeva on 30/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
